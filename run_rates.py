2a613a4bbef0b0633bbdbbac91e1b8fd0f913188
Alexander_Mercaldi <alexander.mercaldi@email.ucr.edu>
2020-11-18 20:20:25 +0100

    minor qol updates to run_rates.py

--- run_rates.py
+++ run_rates.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import argparse
 import os
 import sys
 
 rates_dict = {
-"NuGun" : "RelValNuGun_PU_step1_ALL.root",
+"NuGun" : "RelValNuGun_PU_106X_mcRun3_2021_realistic_v3_rsb-v2.root",
 "QCD"   : "SMP-PhaseIITDRFall17DR-00002_step1_1-10.root",
 "LLP_MH250_Ctau500" : "MH-250_MFF-120_CTau-500mm_step1.root",
 "LLP_MH250_Ctau1000" : "MH-250_MFF-120_CTau-1000mm_step1.root",
@@ -16,8 +16,9 @@ rates_dict = {
 }
 
 def run_rates(name):
-    print("Running rates for "+name)
+    print("\nRunning rates for "+name+"\n")
     os.system("rates.exe "+name+" dir_ntuple/L1Ntuple_"+rates_dict[name])
+    os.system("mv rates_"+name+".root  result_rates/")
 
 
 def main():
@@ -31,6 +32,7 @@ def main():
     parser.add_argument("-m250", action="store_true", default=False, help="Run over LLP with MH=350")
     args = parser.parse_args()
 
+    os.system("mkdir -p result_rates")
     if args.a:
         for name in rates_dict.keys():
             run_rates(name)
@@ -53,15 +55,5 @@ def main():
         run_rates("LLP_MH250_Ctau500")
         run_rates("LLP_MH250_Ctau1000")
 
-    os.system("mkdir -p result_rates")
-    os.system("mv rates*.root result_rates")
-
 if __name__ == "__main__":
     main()
-
-
-
-
-        
-    
-
