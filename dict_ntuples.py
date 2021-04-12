cd62caced8549d43163ceb107833b2923a2fcd92
Alexander_Mercaldi <alexander.mercaldi@email.ucr.edu>
2021-04-12 17:10:29 -0500

    added nutple dictionary

---
+++ dict_ntuples.py
@@ -0,0 +1,19 @@
+dict = {
+
+"QCD"   : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/QCD_Pt-15to3000_TuneCP5_Flat_14TeV_pythia8_HCAL.root",
+"NuGun" : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/Neutrino_Pt-2to20_gun_HCAL.root",
+"RelValNuGun" : "root:://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/L1Ntuple_RelValNuGun_CMSSW_11_0_0_patch1-PU_110X_mcRun3_2021_realistic_v6-v1.root",
+"LLP_MH250_Ctau500" : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/HTo2LongLivedTo4b_MH-250_MFF-120_CTau-500mm_TuneCP5_14TeV_pythia8_HCAL.root",
+"LLP_MH250_Ctau1000" : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/HTo2LongLivedTo4b_MH-250_MFF-120_CTau-1000mm_TuneCP5_14TeV_pythia8_HCAL.root",
+"LLP_MH350_Ctau500" : "root://cmseos.fnal.gov://store/user/amercald/L1_Ntuples_11_X/HTo2LongLivedTo4b_MH-350_MFF-160_CTau-500mm_TuneCP5_14TeV_pythia8_HCAL.root",
+"LLP_MH350_Ctau1000" : "root://cmseos.fnal.gov://store/user/amercald/L1_Ntuples_11_X/HTo2LongLivedTo4b_MH-350_MFF-160_CTau-1000mm_TuneCP5_14TeV_pythia8_HCAL.root",
+"LLP_MH250_60_Ctau500" : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/HTo2LongLivedTo4b_MH-250_MFF-60_CTau-500mm_TuneCP5_14TeV_pythia8_HCAL.root",
+"LLP_MH250_60_Ctau1000" : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/HTo2LongLivedTo4b_MH-250_MFF-60_CTau-1000mm_TuneCP5_14TeV_pythia8_HCAL.root",
+"LLP_MH350_80_Ctau500" : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/HTo2LongLivedTo4b_MH-350_MFF-80_CTau-500mm_TuneCP5_14TeV_pythia8_HCAL.root",
+"LLP_MH350_80_Ctau1000" : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/HTo2LongLivedTo4b_MH-350_MFF-80_CTau-1000mm_TuneCP5_14TeV_pythia8_HCAL.root",
+
+"LLP_MH1000_Ctau500" : "MH-1000_MFF-450_CTau-500mm_step1.root",
+"LLP_MH1000_Ctau1000" : "MH-1000_MFF-450_CTau-1000mm_step1.root",
+
+"test"         : "root://cmseos.fnal.gov://store/user/amercald/L1Ntuples_11_X/L1Ntuple_test_LLP_350_160_1000.root",
+}
